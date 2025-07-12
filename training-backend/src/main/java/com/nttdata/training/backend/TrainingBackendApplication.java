3e2f90d6f47403366711720fe361d572ebbbaf97
ZHEWEN LI <glizhewen@gmail.com>
2025-07-12 16:08:03 +0900

    init

---
+++ training-backend/src/main/java/com/nttdata/training/backend/TrainingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.training.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingBackendApplication.class, args);
+    }
+
+}
---
+++ training-backend/src/test/java/com/nttdata/training/backend/TrainingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.training.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
